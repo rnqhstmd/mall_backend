bc5d842eee8636eb72a23eedc859ca62f70c840b
BonSeung <bs.koo@sqisoft.com>
2024-08-12 17:41:51 +0900

    fix: JpaRepository 제네릭 타입 수정 - #2

--- src/main/java/com/mall/repository/TodoRepository.java
+++ src/main/java/com/mall/repository/TodoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TodoRepository extends JpaRepository<UUID, Todo> {
+public interface TodoRepository extends JpaRepository<Todo, UUID> {
 }
