d9e936ad23f2db77cc5c92f579b020e2ab5afdf5
BonSeung <bs.koo@sqisoft.com>
2024-08-13 10:53:48 +0900

    feat: PageResponseDto 추가 - #4

---
+++ src/main/java/com/mall/dto/page/PageResponseDto.java
@@ -0,0 +1,41 @@
+package com.mall.dto.page;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Getter
+@AllArgsConstructor
+public class PageResponseDto<E> {
+    private List<E> dtoList;
+    private List<Integer> pageNumList;
+    private PageRequestDto pageRequestDTO;
+    private boolean prev, next;
+    private int totalCount, prevPage, nextPage, totalPage, current;
+
+    @Builder(builderMethodName = "withAll")
+    public PageResponseDto(List<E> dtoList, PageRequestDto pageRequestDTO, long totalCount) {
+        this.dtoList = dtoList;
+        this.pageRequestDTO = pageRequestDTO;
+        this.totalCount = (int) totalCount;
+
+        int end = (int) (Math.ceil(pageRequestDTO.getPage() / 10.0)) * 10;
+        int start = end - 9;
+        int last = (int) (Math.ceil((totalCount / (double) pageRequestDTO.getSize())));
+        end = end > last ? last : end;
+
+        this.prev = start > 1;
+        this.next = totalCount > end * pageRequestDTO.getSize();
+        this.pageNumList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+        if (prev)
+            this.prevPage = start - 1;
+        if (next)
+            this.nextPage = end + 1;
+        this.totalPage = this.pageNumList.size();
+        this.current = pageRequestDTO.getPage();
+    }
+}
