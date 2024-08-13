a268fce126a9ffa9bfe0b7b2381ed7ae15cf0761
BonSeung <bs.koo@sqisoft.com>
2024-08-13 10:55:46 +0900

    feat: TodoService 추가 - #4

---
+++ src/main/java/com/mall/service/TodoService.java
@@ -0,0 +1,78 @@
+package com.mall.service;
+
+import com.mall.domain.Todo;
+import com.mall.dto.page.PageRequestDto;
+import com.mall.dto.page.PageResponseDto;
+import com.mall.dto.todo.TodoDto;
+import com.mall.exception.ErrorCode;
+import com.mall.exception.NotFoundException;
+import com.mall.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TodoService {
+    private final TodoRepository todoRepository;
+    private final ModelMapper modelMapper;
+
+    public UUID register(TodoDto todoDto) {
+        Todo todo = modelMapper.map(todoDto, Todo.class);
+        Todo save = todoRepository.save(todo);
+        return save.getId();
+    }
+
+    public TodoDto get(UUID tno) {
+        Todo todo = todoRepository.findById(tno).orElseThrow(() -> new NotFoundException(ErrorCode.TODO_NOT_FOUND));
+        TodoDto dto = modelMapper.map(todo, TodoDto.class);
+        return dto;
+    }
+
+    public void modify(UUID tno, TodoDto todoDto) {
+        Todo todo = todoRepository.findById(tno)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.TODO_NOT_FOUND));
+
+        todo.changeTitle(todoDto.getTitle());
+        todo.changeDueDate(todoDto.getDueDate());
+        todo.changeComplete(todoDto.isComplete());
+
+        todoRepository.save(todo);
+    }
+
+    public void remove(UUID tno) {
+        Todo todo = todoRepository.findById(tno)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.TODO_NOT_FOUND));
+        todoRepository.deleteById(todo.getId());
+    }
+
+    public PageResponseDto<TodoDto> list(PageRequestDto pageRequestDto) {
+        Pageable pageable = PageRequest.of(
+                pageRequestDto.getPage() - 1,
+                pageRequestDto.getSize(),
+                Sort.by("createdAt").descending());
+        Page<Todo> todoList = todoRepository.findAll(pageable);
+
+        List<TodoDto> dtoList = todoList.getContent().stream()
+                .map(todo -> modelMapper.map(todo, TodoDto.class))
+                .collect(Collectors.toList());
+
+        long totalCount = todoList.getTotalElements();
+
+        PageResponseDto<TodoDto> responseDto = PageResponseDto.<TodoDto>withAll()
+                .dtoList(dtoList)
+                .pageRequestDTO(pageRequestDto)
+                .totalCount(totalCount)
+                .build();
+
+        return responseDto;
+    }
+}
