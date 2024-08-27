0cbd0b2156ee524fae820ba92ce5f4d0953ab6b2
BonSeung <bs.koo@sqisoft.com>
2024-08-27 14:05:41 +0900

    feat: 이메일로 회원 객체 반환 메서드 추가 - #12

--- src/main/java/com/mall/repository/MemberRepository.java
+++ src/main/java/com/mall/repository/MemberRepository.java
@@ -1,9 +1,15 @@
 package com.mall.repository;
 
 import com.mall.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
+    @EntityGraph(attributePaths = {"memberRoleList"})
+    @Query("select m from Member m where m.email = :email")
+    Member getWithRoles(@Param("email") String email);
 }
