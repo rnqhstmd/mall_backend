d8ca18a344ea5784eef6ca58ae71220747e0a95b
BonSeung <bs.koo@sqisoft.com>
2024-08-27 14:01:22 +0900

    feat:  폼 기반 인증을 사용자 정의 방식으로 설정 - #12

--- src/main/java/com/mall/config/CustomSecurityConfig.java
+++ src/main/java/com/mall/config/CustomSecurityConfig.java
@@ -1,15 +1,20 @@
 package com.mall.config;
 
+import com.mall.security.filter.JwtCheckFilter;
+import com.mall.security.handler.LoginApiFailHandler;
+import com.mall.security.handler.LoginApiSuccessHandler;
 import jakarta.persistence.Table;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,6 +23,7 @@ import java.util.Arrays;
 
 @Slf4j
 @Configuration
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
     // 보안 관련 요청 가로채고 처리하는 보안 필터 체인 설정
@@ -29,9 +35,16 @@ public class CustomSecurityConfig {
         });
 
         httpSecurity.sessionManagement(sessionConfig ->
-                sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                sessionConfig.sessionCreationPolicy(SessionCreationPolicy.NEVER));
         httpSecurity.csrf(config -> config.disable());
 
+        httpSecurity.formLogin(config -> {
+            config.loginPage("/api/member/login");
+            config.successHandler(new LoginApiSuccessHandler());
+            config.failureHandler(new LoginApiFailHandler());
+        });
+
+        httpSecurity.addFilterBefore(new JwtCheckFilter(), UsernamePasswordAuthenticationFilter.class); // JWT 체크
         return httpSecurity.build();
     }
 
