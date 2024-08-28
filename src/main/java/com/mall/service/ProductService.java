681a777389ec8cbde2c55f7ad7367522ba557e06
BonSeung <bs.koo@sqisoft.com>
2024-08-28 09:34:11 +0900

    feat: 상품 등록, 조회 기능 추가 - #14

--- src/main/java/com/mall/service/ProductService.java
+++ src/main/java/com/mall/service/ProductService.java
@@ -5,6 +5,8 @@ import com.mall.domain.ProductImage;
 import com.mall.dto.page.PageRequestDto;
 import com.mall.dto.page.PageResponseDto;
 import com.mall.dto.product.ProductDto;
+import com.mall.exception.ErrorCode;
+import com.mall.exception.NotFoundException;
 import com.mall.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,6 +26,41 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public void register(ProductDto productDto) {
+        Product product = Product.builder()
+                .pname(productDto.getPname())
+                .pdesc(productDto.getPdesc())
+                .price(productDto.getPrice())
+                .build();
+
+        List<String> uploadFileNames = productDto.getUploadFileNames();
+
+        uploadFileNames.stream().forEach(uploadName -> {
+            product.addImageString(uploadName);
+        });
+
+        productRepository.save(product);
+    }
+
+    public ProductDto getProductById(UUID productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NotFoundException(ErrorCode.PRODUCT_NOT_FOUND));
+        ProductDto productDto = ProductDto.builder()
+                .productId(String.valueOf(productId))
+                .pname(product.getPname())
+                .pdesc(product.getPdesc())
+                .price(product.getPrice())
+                .build();
+        List<ProductImage> imageList = product.getImageList();
+        if(imageList == null || imageList.size() == 0 ){
+            return productDto;
+        }
+        List<String> fileNameList = imageList.stream().map(productImage ->
+                productImage.getOrd()+"_"+productImage.getFileName()).toList();
+        productDto.setUploadFileNames(fileNameList);
+
+        return productDto;
+    }
 
     public PageResponseDto<ProductDto> getList(PageRequestDto pageRequestDto) {
         Pageable pageable = PageRequest.of(
