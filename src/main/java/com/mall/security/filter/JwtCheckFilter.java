2e22db028483886b02340a8bbc401328c6a1928b
BonSeung <bs.koo@sqisoft.com>
2024-08-28 10:23:52 +0900

    feat: JWT 인증 정보를 이용해서 사용자 검증 로직 추가 - #16

--- src/main/java/com/mall/security/filter/JwtCheckFilter.java
+++ src/main/java/com/mall/security/filter/JwtCheckFilter.java
@@ -1,6 +1,7 @@
 package com.mall.security.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mall.dto.member.MemberDto;
 import com.mall.exception.ErrorCode;
 import com.mall.exception.UnauthorizedException;
 import com.mall.util.JwtUtil;
@@ -8,9 +9,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class JwtCheckFilter extends OncePerRequestFilter {
@@ -38,7 +42,18 @@ public class JwtCheckFilter extends OncePerRequestFilter {
         String authHeader = request.getHeader("Authorization");
         try {
             String accessToken = authHeader.substring(7);
+
             Map<String, Object> claims = JwtUtil.validateToken(accessToken);
+            String email = (String) claims.get("email");
+            String pw = (String) claims.get("pw");
+            String nickname = (String) claims.get("nickname");
+            Boolean social = (Boolean) claims.get("social");
+            List<String> roleNames = (List<String>) claims.get("roleNames");
+
+            MemberDto memberDto = new MemberDto(email, pw, nickname, social, roleNames);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(memberDto, pw, memberDto.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
             filterChain.doFilter(request, response);
         } catch (Exception e) {
             throw new UnauthorizedException(ErrorCode.INVALID_TOKEN);
